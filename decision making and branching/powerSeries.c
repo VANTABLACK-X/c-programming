1a78273c8f31145b6b0c00c1b8d8015e627ff09c
Ninja <muhamadusulaiman41912@gmail.com>
2022-01-28 08:07:21 -0800

    update

--- decision making and branching/powerSeries.c
+++ decision making and branching/powerSeries.c
@@ -4,5 +4,25 @@
 
 int main()
 {
-    
+    int count,n;
+    float term,sum,x;
+    printf("enter the value ");
+    scanf("%f",&x);
+    count=n=term=sum=1;
+    while (n<=100)
+    {
+        term=(float)term*(x/n);
+        sum=(float)sum+term;
+        count=count+1;
+        if (term<accurancy)
+        {
+            n=999;
+        }
+        else
+        {
+            n=n+1;
+        }   
+    }
+    printf("x = %.1f\nterm = %d\nsum = %f",x,count,sum);
+    return 0;
 } 
