d19aeca6f5f86a53d625b4b0a719fa6ef24801d7
Ninja <muhamadusulaiman41912@gmail.com>
2022-02-09 22:55:07 -0800

    upload

---
+++ 5_decision_macking_and_looping/2_to_power_n_and_-n.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int n;
+    long int p;
+    double q;
+
+    printf("_____________________________________________________\n");
+    printf("  2 to power n      n       2 to power -n\n");
+    printf("_____________________________________________________\n");
+
+    for (n=0;n<21;n++)
+    {
+        if(n==0)
+        p=1;
+        else
+        p=p*2;
+        q=0.1/(double)p;
+        printf("%10d %10d %20.12lf\n",p,n,q);
+    }
+    printf("_____________________________________________________\n");
+    return 0;
+}
