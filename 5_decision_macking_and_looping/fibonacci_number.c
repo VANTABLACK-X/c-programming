5f35649432da8092342563d0fd3b8f5927ca68a0
Ninja <muhamadusulaiman41912@gmail.com>
2022-02-14 05:37:12 -0800

    upload

--- 5_decision_macking_and_looping/fibonacci_number.c
+++ 5_decision_macking_and_looping/fibonacci_number.c
@@ -14,5 +14,20 @@ int main()
         n2=fib;
         printf("%4d",fib);
     }
+    printf("\nusing Do while statement :");
+    fib=0;
+    n1=0;
+    n2=1;
+    printf("%4d%4d",n1,n2);
+    i=1;
+    do
+    {
+        fib=n1+n2;
+        n1=n2;
+        n2=fib;
+        printf("%4d",fib);
+        i=i+1;
+    } while (i<=n-2);
+    
     return 0;
 }
