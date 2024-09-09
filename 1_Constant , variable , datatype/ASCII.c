d8adb53d8ebfab00b32d938694ba79fbada1aaa1
VANTABLACK.ORG.IN <83003977+VANTABLACK-X@users.noreply.github.com>
2024-09-09 11:02:42 +0530

    update

--- 1_Constant , variable , datatype/ASCII.c
+++ 1_Constant , variable , datatype/ASCII.c
@@ -3,7 +3,7 @@
 int main()
 {
  char a;
- printf("enter the charecter to convert into ASCII :");
+ printf("enter the character to convert into ASCII :");
  scanf("%c",&a);
  printf("ASCII value for %c is %d",a,a);
  return 0;
